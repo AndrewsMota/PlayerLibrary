48c9fddde672e371bef168ebb7abb73a25c6e28e
Andrews <andrewscontato1@gmail.com>
2025-02-13 20:07:49 -0300

    feat: Verifica se o nome passado no request é josue

--- Features/HelloHandler.cs
+++ Features/HelloHandler.cs
@@ -6,9 +6,12 @@ namespace HelloApi.Features;
 public class HelloHandler : IRequestHandler<HelloRequest, Result<string>>
 {
 	public Task<Result<string>> Handle(HelloRequest request, CancellationToken cancellationToken)
-	{ 
+	{
 		if(string.IsNullOrEmpty(request.Name))
 			return Task.FromResult(Result.Fail<string>("O nome não pode ser vazio."));
+		
+		if(request.Name == "josue")
+			return Task.FromResult(Result.Fail<string>("JOSUE DETECTADO"));
 		
 		return Task.FromResult(Result.Ok($"Hello, {request.Name}!"));
 	}
